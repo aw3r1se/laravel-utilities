b828c973370897bc409cc195c60135ac1908ba47
koch <koch@echo-company.ru>
2023-04-12 21:45:21 +0500

    renamed migrations

--- src/Providers/UtilityTraitsServiceProvider.php
+++ src/Providers/UtilityTraitsServiceProvider.php
@@ -10,11 +10,11 @@ class UtilityTraitsServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../../database/migrations/create_images_table.php' =>
+                __DIR__ . '/../../database/migrations/2023_01_01_000001_create_images_table.php' =>
                     database_path('migrations/2023_01_01_000001_create_images_table.php'),
-                __DIR__ . '/../../database/migrations/create_gallery_images_table.php' =>
+                __DIR__ . '/../../database/migrations/2023_01_01_000002_create_gallery_images_table.php' =>
                     database_path('migrations/2023_01_01_000002_create_images_table.php'),
-                __DIR__ . '/../../database/migrations/create_documents_table.php' =>
+                __DIR__ . '/../../database/migrations/2023_01_01_000003_create_documents_table.php' =>
                     database_path('migrations/2023_01_01_000003_create_images_table.php'),
             ], 'migrations');
         }
